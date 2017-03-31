af22e0a4570187e7de431473945b10ff613178b0
Anartz Mugika <mugan86@gmail.com>
2017-03-31 13:26:43 +0200

    Get UUID to use in new static campaigns

--- src/app/_services/campaign.service.ts
+++ src/app/_services/campaign.service.ts
@@ -46,14 +46,30 @@ export class CampaignService {
     {
         let campainList : Campaign[];
         campainList = [
-            {"name": "hfhfhf", "token": "eeeee", "totalBanners": 23, "activeBanners": 24 },
-            {"name": "hdfhfhfhghhfg", "token": "csfffff", "totalBanners": 556, "activeBanners": 12 },
-            {"name": "aaaaaa", "token": "iwiwkwoslls", "totalBanners": 76, "activeBanners": 86 }
+            {"name": this.createUUID(false), "token": this.createUUID(true), "totalBanners": 23, "activeBanners": 24 },
+            {"name": this.createUUID(false), "token": this.createUUID(true), "totalBanners": 556, "activeBanners": 12 },
+            {"name": this.createUUID(false), "token": this.createUUID(true), "totalBanners": 76, "activeBanners": 86 }
         ];
 
         let action = {type: "ADD_STATIC", payload: campainList};
         console.log(action);
         this.store.dispatch(action);
+    }
+
+    private  createUUID(withNumberAndChars) {
+        // http://www.ietf.org/rfc/rfc4122.txt
+        var s = [];
+        var hexDigits = "0123456789abcdef";
+        if (!withNumberAndChars) hexDigits = "abcdef";
+        for (var i = 0; i < 36; i++) {
+            s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+        }
+        s[14] = "4";  // bits 12-15 of the time_hi_and_version field to 0010
+        s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);  // bits 6-7 of the clock_seq_hi_and_reserved to 01
+        s[8] = s[13] = s[18] = s[23] = "-";
+
+        var uuid = s.join("");
+        return uuid;
     }
 
     hello()
